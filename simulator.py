e6fb4702fbf65a59a143f3c485c7571a6bbb2d99
anjou0618 <joker20020618@gmail.com>
2026-06-29 16:56:48 +0900

    simulator init changed(added occupancy init)

--- simulator.py
+++ simulator.py
@@ -8,7 +8,13 @@ class Simulator:
     def __init__(self, map_data: MapData) -> None:
         self.map_data = map_data
         self.drones: list[Drone] = []
+        self.zone_occupancy = {
+                name: 0 for name in self.map_data.zones
+        }
+        self.current_turn: int = 1
 
+        start_name = self.map_data.start_hub.name
+        self.zone_occupancy[start_name] = self.map_data.nb_drones
         for i in range(1, self.map_data.nb_drones + 1):
             drone = Drone(
                     i,
